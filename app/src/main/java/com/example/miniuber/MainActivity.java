92098d7960d4c25bb77eedb2b51a341129f00c93
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-04-15 22:39:07 +0200

    Modules Construction

--- app/src/main/java/com/example/miniuber/MainActivity.java
+++ app/src/main/java/com/example/miniuber/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.miniuber;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler handler ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.module_selector);
+        getSupportActionBar().hide();
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, ModuleSelectorActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+
+                finish();
+            }
+        },2000);
     }
 }
---
+++ app/src/main/java/com/example/miniuber/ModuleSelectorActivity.java
@@ -0,0 +1,16 @@
+package com.example.miniuber;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModuleSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
