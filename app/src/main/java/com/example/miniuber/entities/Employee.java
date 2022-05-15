fab9cf951e3cec2b4f04a8e397c9f98f98470dfc
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-05-16 01:59:47 +0200

    Revert "Merge branch 'master' of https://github.com/ahmedmaged3k1/MiniUber"
    This reverts commit 5b0dfed5a12092b9f7cd3eb6cda401c2f842725c, reversing
    changes made to f283436f1eccc0c1c727cd4c909cd6b127275e2f.

--- app/src/main/java/com/example/miniuber/entities/Employee.java
+++
@@ -1,9 +0,0 @@
-package com.example.miniuber.entities;
-
-public class Employee extends User{
-
-        public Employee(String name, String email, String phoneNumber, String userId, float rate) {
-            super(name, email, phoneNumber, userId, rate);
-        }
-
-}
